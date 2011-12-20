73e554d6b02dacd51d5f2f7bf6282b988d1cecb8
Taylor <Taylor@0a8336b3-7eeb-364f-9013-38a028100c4a>
2011-12-20 19:23:55 +0000

    Branching for bringup of new WODSEF boards.  I will merge this back in whenever we have a sufficient number of new boards working.

---
+++ Avatar_Controller/Controller_V2/Carrier_Board/branches/WODSEF/src/stdhdr.h
@@ -0,0 +1,36 @@
+/**
+ * @file stdhdr.h
+ * @author J. Brinton
+ * @author Robotex, Inc.
+ *
+ * Robotex one-size-fits-all firmware. Designed for the PIC24FJ256GB106 only.
+ *
+ */
+
+#ifndef STDHDR_H
+#define STDHDR_H
+
+
+#define FCY 16000000UL        // instruction clock
+#define I2C_RATE_SETTING 0Xff // 200 KHz for 32 MHz clock frequency
+
+#include <stdint.h>
+#include <libpic30.h>
+
+#include "GenericTypeDefs.h"
+#include "Compiler.h"
+#include "USB/usb.h"
+#include "i2c.h"
+#include "PwrMgnt.h"
+#include "interrupt_switch.h"
+
+// PROTOTYPES FOR PROJECT stdfunction.h
+
+extern void block_ms(unsigned int ms);
+extern void writeI2C( unsigned char add, unsigned char v );
+extern int readI2C( unsigned char add );
+extern void writeI2CReg( unsigned char add, unsigned char v, unsigned char w);
+
+#include "../HardwareProfile.h"
+
+#endif
---
+++ Avatar_Controller/Controller_V2/Carrier_Board/branches/WODSEF/src/usb_descriptors.c
@@ -0,0 +1,107 @@
+ /**
+ * @file usb_descriptors.c
+ * @author Joel Brinton
+ * @author Robotex, Inc.
+ *
+ * Microchip USB driver settings
+ *
+ */
+
+#include <USB/usb.h>
+#include <usb_config.h>
+
+/* Device Descriptor */
+USB_DEVICE_DESCRIPTOR device_dsc=
+{
+    0x12,                         // Size of this descriptor in bytes
+    USB_DESCRIPTOR_DEVICE,        // DEVICE descriptor type
+    0x0200,                       // USB Spec Release Number in BCD format        
+    0x00,                         // Class Code
+    0x00,                         // Subclass code
+    0x00,                         // Protocol code
+    USB_EP0_BUFF_SIZE,            // Max packet size for EP0, see usb_config.h
+    0xB1E1,                       // Vendor ID: Brinton Engineering LLC
+    0x0001,                       // Product ID: Robotex OCU
+    0x0000,                       // Device release number in BCD format
+    0x01,                         // Manufacturer string index
+    0x02,                         // Product string index
+    0x00,                         // Device serial number string index
+    0x01                          // Number of possible configurations
+};
+
+/* Configuration 1 Descriptor */
+ROM BYTE configDescriptor1[]={
+    /* Configuration Descriptor */
+    0x09,                         // Size of this descriptor in bytes
+    USB_DESCRIPTOR_CONFIGURATION, // CONFIGURATION descriptor type
+    0x20,0x00,                    // Total length of data for this cfg
+    1,                            // Number of interfaces in this cfg
+    1,                            // Index value of this configuration
+    0,                            // Configuration string index
+    _DEFAULT | _SELF,             // Attributes, see usb_device.h
+    50,                           // Max power consumption (2X mA)
+							
+    /* Interface Descriptor */
+    0x09,//sizeof(USB_INTF_DSC),  // Size of this descriptor in bytes
+    USB_DESCRIPTOR_INTERFACE,     // INTERFACE descriptor type
+    0,                            // Interface Number
+    0,                            // Alternate Setting Number
+    2,                            // Number of endpoints in this intf
+    0xFF,                         // Class code
+    0xFF,                         // Subclass code
+    0xFF,                         // Protocol code
+    0,                            // Interface string index
+    
+    /* Endpoint Descriptor */
+    0x07,                         // sizeof(USB_EP_DSC)
+    USB_DESCRIPTOR_ENDPOINT,      // Endpoint Descriptor
+    _EP01_OUT,                    // EndpointAddress
+    _ISO,                         // Attributes
+//    OUT_PACKET_LENGTH & 0xFF,
+//    (OUT_PACKET_LENGTH>>8) & 0xFF,// size
+    0xFF, 0xFF,
+    1,                            // Interval
+    
+    0x07,                         // sizeof(USB_EP_DSC)
+    USB_DESCRIPTOR_ENDPOINT,      // Endpoint Descriptor
+    _EP01_IN,                     // EndpointAddress
+    _ISO,                         // Attributes
+//    IN_PACKET_LENGTH & 0xFF,
+//    (IN_PACKET_LENGTH>>8) & 0xFF, // size
+    0xFF, 0xFF,
+    1                             // Interval
+};
+
+
+// Language code string descriptor
+ROM struct{BYTE bLength;BYTE bDscType;WORD string[1];}sd000={
+sizeof(sd000),USB_DESCRIPTOR_STRING,{0x0001}};
+
+// Manufacturer string descriptor
+ROM struct{BYTE bLength;BYTE bDscType;WORD string[23];}sd001={
+sizeof(sd001),USB_DESCRIPTOR_STRING,
+{'B','r','i','n','t','o','n',' ',
+ 'E','n','g','i','n','e','e','r','i','n','g',' ',
+ 'L','L','C'}};
+
+// Product string descriptor
+ROM struct{BYTE bLength;BYTE bDscType;WORD string[26];}sd002={
+sizeof(sd002),USB_DESCRIPTOR_STRING,
+{'R','o','b','o','t','e','x',' ',
+ 'R','o','b','o','t','i','c',' ',
+ 'C','o','n','t','r','o','l','l','e','r'}};
+
+// Array of configuration descriptors
+ROM BYTE *ROM USB_CD_Ptr[]=
+{
+    (ROM BYTE *ROM)&configDescriptor1
+};
+
+// Array of string descriptors
+ROM BYTE *ROM USB_SD_Ptr[]=
+{
+    (ROM BYTE *ROM)&sd000,
+    (ROM BYTE *ROM)&sd001,
+    (ROM BYTE *ROM)&sd002
+};
+
