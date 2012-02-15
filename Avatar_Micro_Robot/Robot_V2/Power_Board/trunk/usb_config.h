65404a1b6e63a0824789c9b5f02d22a8f2c4fa67
Taylor <Taylor@0a8336b3-7eeb-364f-9013-38a028100c4a>
2012-02-15 05:05:33 +0000

    Changed packet size to 255.

--- Avatar_Micro_Robot/Robot_V2/Power_Board/trunk/usb_config.h
+++ Avatar_Micro_Robot/Robot_V2/Power_Board/trunk/usb_config.h
@@ -49,8 +49,8 @@ typedef int16_t DEVICE_NUMBER;
 #define SYNC    1
 #define NO_SYNC 0
 
-#define OUT_PACKET_LENGTH 980
-#define IN_PACKET_LENGTH  980
+#define OUT_PACKET_LENGTH 255
+#define IN_PACKET_LENGTH  255
 
 #define PACKET_TERMINATOR 0xFFFF
 
