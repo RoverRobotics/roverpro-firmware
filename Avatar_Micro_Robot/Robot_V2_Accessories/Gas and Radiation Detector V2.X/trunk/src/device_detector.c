67a69cfc4f288b93bb0ab683eb0fddf90804bb2e
Taylor <Taylor@0a8336b3-7eeb-364f-9013-38a028100c4a>
2012-04-27 19:55:59 +0000

    Added code to turn on the power bus.  This should be all the firmware we need until we want to add more features.

--- Avatar_Micro_Robot/Robot_V2_Accessories/Gas and Radiation Detector V2.X/trunk/src/device_detector.c
+++ Avatar_Micro_Robot/Robot_V2_Accessories/Gas and Radiation Detector V2.X/trunk/src/device_detector.c
@@ -1,11 +1,18 @@
 
-
 #include "stdhdr.h"
 #include "device_detector.h"
 
+#define POWER_BUS_EN(a)   (_TRISB0 = !a)
+#define POWER_BUS_ON(a)   (_LATB0 = a)
+
 void DeviceDetectorInit()
 {
-  REG_CARRIER_STATE = 0;
+   
+  POWER_BUS_ON(0);
+  POWER_BUS_EN(1);
+  block_ms(200);
+  POWER_BUS_ON(1);
+
 
 }
 
