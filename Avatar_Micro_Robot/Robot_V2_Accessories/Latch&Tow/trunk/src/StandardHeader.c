8b9dc29c7e715b3700602df58b719a7733cefbe3
SLeventis <SLeventis@0a8336b3-7eeb-364f-9013-38a028100c4a>
2012-06-04 22:36:51 +0000

    - (small) Standard header file to organize common dependencies, macros,   type definitions, general functions and other generally useful facilities   across an application

---
+++ Avatar_Micro_Robot/Robot_V2_Accessories/Latch&Tow/trunk/src/StandardHeader.c
@@ -0,0 +1,59 @@
+/*=============================================================================
+File: StandardHeader.c
+=============================================================================*/
+//#define TEST_STANDARD_HEADER
+/*---------------------------Dependencies------------------------------------*/
+#include "./StandardHeader.h"
+
+/*---------------------------Macros------------------------------------------*/
+#define OPS_PER_MS            3200  // operations per millisecond, for current 
+                                    // oscillator choice and 
+                                    // configuration-bit settings
+
+/*---------------------------Helper Function Prototypes----------------------*/
+
+/*---------------------------Test Harness------------------------------------*/
+#ifdef TEST_STANDARD_HEADER
+int main(void) {
+  _TRISE5 = OUTPUT; _RE5 = 0;       // configure a debugging pin
+  
+  
+  unsigned char i = 0;
+	while (1) {
+  	/*
+  	// test Pause()
+  	Pause(500);
+  	_RE5 ^= 1;
+  	*/
+  	
+  	// test Map() (with Pause())
+		Pause(Map(i++, 20, 80, 0, 100));
+		_RE5 ^= 1;                      // toggle a pin to view an output 
+	}
+
+	return 0;
+}
+#endif
+/*---------------------------End Test Harness--------------------------------*/
+/*---------------------------Public Function Definitions---------------------*/
+void Delay(unsigned int milliseconds) {
+  unsigned int i,j;
+	for (i = 0; i < OPS_PER_MS; i++) {
+    for (j = 0; j < milliseconds; j++);
+  }
+}
+
+unsigned int Map(int value, int from_low, int from_high, 
+                 int to_low, int to_high) {
+  // compute the linear interpolation
+  unsigned int result = ((double)(value - from_low) / (double)(from_high - from_low))
+                        * (double)(to_high - to_low) + to_low;
+  
+  // constrain the result to within a valid output range
+  if (to_high < result) result = to_high;
+  else if (result < to_low) result = to_low;
+  
+  return result;
+}
+
+/*---------------------------Helper Function Definitions---------------------*/
