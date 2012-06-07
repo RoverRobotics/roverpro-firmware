605efc9dc2e617af97ddb87a007704dd4b84f311
SLeventis <SLeventis@0a8336b3-7eeb-364f-9013-38a028100c4a>
2012-06-07 03:30:37 +0000

    - added firmware for a calibration tool for the off-the-shelf electronic speed controller

---
+++ Avatar_Micro_Robot/Robot_V2_Accessories/Latch&Tow/trunk/SupportingFiles/ESC_Calibrator/ESC_Calibrator.ino
@@ -0,0 +1,71 @@
+/*=============================================================================
+ 
+File: ESC_Calibrator.ino
+
+Description: This file calibrates a LOSI electronic speed controller (ESC) for
+  our current setup.  It employs the PWM library to set neutral, high and low
+  pulse-width thresholds (1.5ms, 2ms and 1ms pulse-widths respectively) at a 
+  frequency of about 100Hz.
+
+Responsible Engineer: Stellios Leventis  (sleventis@robotex.com)
+=============================================================================*/
+/*---------------------------Dependencies------------------------------------*/
+#include <PWM.h>
+#include <Timers.h>
+
+/*---------------------------Macros------------------------------------------*/
+#define MAX_DC               20    // 0.20 * 10ms = 2ms pulse width
+#define NEUTRAL_DC           15
+#define MIN_DC               10
+#define PWM_PIN              11    // must be either 3 or 11
+
+// times and timers
+#define _100ms               (100)
+#define HEARTBEAT_TIMER      0
+#define HEARTBEAT_HALFPERIOD (500)
+#define TEST_TIMER           1
+#define CALIBRATION_TIME    (50*_100ms)
+
+#define HEARTBEAT_LED        13    // the onboard LED
+
+/*---------------------------Public Function Definitions---------------------*/
+void setup() {
+  pinMode(HEARTBEAT_LED, OUTPUT); digitalWrite(HEARTBEAT_LED, LOW);
+  
+  InitPWM(1250);                  // T = 1250 * 8us => 10ms => f = 100Hz
+
+  StartTimer(HEARTBEAT_TIMER, HEARTBEAT_HALFPERIOD);
+  StartTimer(TEST_TIMER, CALIBRATION_TIME);
+}
+
+void loop() {
+  // toggle the onboard LED to indicate normal operation
+  if (IsTimerExpired(HEARTBEAT_TIMER)) {
+    StartTimer(HEARTBEAT_TIMER, HEARTBEAT_HALFPERIOD);
+    static unsigned char alternator = 0;
+    digitalWrite(HEARTBEAT_LED, (++alternator % 2) ? LOW : HIGH);
+  }
+  
+  if (IsTimerExpired(TEST_TIMER)) {
+    StartTimer(TEST_TIMER, CALIBRATION_TIME);
+    static unsigned char procedure_step = 0;
+    switch (procedure_step++) {
+      case 0:  // pull to high
+        UpdateDutyCycle(PWM_PIN, NEUTRAL_DC);
+        break;
+      case 1:
+        UpdateDutyCycle(PWM_PIN, MAX_DC);
+        break;
+      case 2:
+        UpdateDutyCycle(PWM_PIN, MIN_DC);
+        break;
+      case 3:
+        UpdateDutyCycle(PWM_PIN, NEUTRAL_DC);
+        digitalWrite(HEARTBEAT_LED, HIGH);
+        while (1);  // stop here and wait to be unplugged
+        break;
+    }
+  }
+  
+}
+
