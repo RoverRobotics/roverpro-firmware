c4ef301da65de064b658aa3e65c3bbd8791dbf2f
Taylor <Taylor@0a8336b3-7eeb-364f-9013-38a028100c4a>
2012-07-19 02:33:03 +0000

    Added back in shell script that compiled and called the arm c++ program.

---
+++ Avatar_Micro_Robot/Robot_V2_Accessories/Robotic_Arm_V2/Testing/ArmTest/source/shell_scripts/handle_arm_test.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# BUG ALERT: if compilation step fails, it will just run old version of code
+
+#get path of current script
+shell_script=`readlink -f "$0"`
+
+
+# allow FS PIC to work through hub.  This will break HS device functionality.
+# cd /sys/bus/pci/drivers/ehci_hcd/
+# sudo sh -c 'find ./ -name "0000:00:*" -print| sed "s/\.\///">unbind'
+
+# figure out which device ehci_hcd is using, and remove it
+cd /sys/bus/pci/drivers/ehci_hcd
+for device in *; do
+  if test `echo $device | grep -c "0000:"` -eq 0; then
+    echo "this is not the device we're looking for";
+  else
+    echo "Removing " $device
+    sudo su -c "echo '1' > /sys/bus/pci/devices/$device/remove"
+  fi
+done
+
+# compile the program
+current_folder=`dirname $shell_script`
+cd $current_folder
+cd ..
+sudo make
+
+echo "Hit Enter"
+read dummy
+
+# run the program
+sudo ./ArmTest $1
