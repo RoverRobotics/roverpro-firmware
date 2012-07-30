165173f8d4a6015af75489961100214f3d6511b2
SLeventis <SLeventis@0a8336b3-7eeb-364f-9013-38a028100c4a>
2012-07-30 22:43:12 +0000

    headers to facilitate interaction of I2C devices (macros for register addresses etc)

---
+++ Avatar_Micro_Robot/Robot_V2/Carrier_Board/branches/I2CTesting/src/I2CDeviceHeaders/ADXL345.h
@@ -0,0 +1,23 @@
+/*******************************************************************************
+File: ADXL345.h
+
+Description: This module provides macros to facilitate access to configuration
+  bit and registers within the ADXL345 accelerometer.
+
+TODO: finish and organize in a useful way
+*******************************************************************************/
+#ifndef ADXL345_H
+#define ADXL345_H
+/*---------------------------Macros-------------------------------------------*/
+
+#define ADXL345_ADDRESS           0x53
+
+#define ADXL345_POWER_CTL_REG     0x2d
+#define ADXL345_DATA_FORMAT_REG   0x31
+#define ADXL345_MEASURE_BIT       4
+#define ADXL345_FULL_RES_BIT      4
+#define ADXL345_RANGE_BIT0        0
+#define ADXL345_RANGE_BIT1        1
+
+
+#endif
