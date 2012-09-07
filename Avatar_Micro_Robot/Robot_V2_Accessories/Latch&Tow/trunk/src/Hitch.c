049b117aeb832148584e6e74409222561fa77c33
SLeventis <SLeventis@0a8336b3-7eeb-364f-9013-38a028100c4a>
2012-09-07 23:50:58 +0000

    moving to using core libraries and reduced USB files in case error with old timers (data types didn't match) seems to get stuck latched now although logic hasn't changed

--- Avatar_Micro_Robot/Robot_V2_Accessories/Latch&Tow/trunk/src/Hitch.c
+++ Avatar_Micro_Robot/Robot_V2_Accessories/Latch&Tow/trunk/src/Hitch.c
@@ -1,25 +1,38 @@
-/*=============================================================================
+/*==============================================================================
 File: Hitch.c
-=============================================================================*/
-//#define TEST_HITCH
-/*---------------------------Dependencies------------------------------------*/
-#include "./Hitch.h"
-#include "./Timers.h"
-#include "./ADC.h"
-#include "./PWM.h"
-#include "./stdhdr.h"         // to get access to our custom registers
-#include "./StandardHeader.h" // for pic header file, Map() function, etc
-#include <stdbool.h>          // for bool data type definition
 
-/*---------------------------Wiring Macros-----------------------------------*/
+Description: This is the overarching file that encapsulates the 
+  application-level firmware for the hitch.  It integrates any dependent 
+  firmware modules and interfaces with external components.
+  
+Notes:
+  - The brown wire for the ESC is GND
+  - the ESC must pause for 2seconds after braking before it can change 
+    direction
+  
+Responsible Engineer: Stellios Leventis (sleventis@robotex.com)
+==============================================================================*/
+#define TEST_HITCH
+/*---------------------------Dependencies-------------------------------------*/
+#include "./core/StandardHeader.h"
+#include "./core/Timers.h"
+#include "./core/ADC.h"
+#include "./core/PWM.h"
+#include "./USB/RXUSBDevice.h"        // for USB firmware/software-shared 
+                                      // registers
+#include <stdlib.h>                   // for abs() function
+
+/*---------------------------Macros-------------------------------------------*/
+#define HITCH_PRODUCT_ID      0x000F  // product ID for USB
+
 // PWM Pin
 #define LATCH_PWM_PIN         2       // RP2
 #define T_PWM                 10      // [ms], period of the PWM signal
 #define MAX_DC                0.20    // t_HI = 2ms
-#define MAX_ALLOWABLE_DC      0.158
+#define MAX_ALLOWABLE_DC      0.19
 #define NEUTRAL_DC            0.15    // t_HI = 1.5ms
-#define MIN_ALLOWABLE_DC      0.142
-#define MIN_DC                0.10    // t_HI = 1ms
+#define MIN_ALLOWABLE_DC      0.100   //0.142  // TODO: min having trouble since calibrated not exactly 1.5ms
+#define MIN_DC                0.13    // t_HI = 1ms
 
 // Power Bus
 #define CONFIG_POWER_BUS(a)   (_TRISD9 = (a))
@@ -52,20 +65,24 @@ File: Hitch.c
 #define UPDATE_TIMER          1
 #define UPDATE_TIME           _100ms  // update software-register interfaces every ~10Hz
 #define TRANSITION_TIMER      2
-#define TRANSITION_TIME       (10*_100ms)
+#define TRANSITION_TIME       (20*_100ms) // ESC needs ~2a to transition from positive to negative
 #define JAM_TIMER             3
 #define JAM_CHECK_PERIOD      (5*_100ms)
 #define UNSAFE_CURRENT_TIMER  4
 #define UNSAFE_CURRENT_TIME   (5*_10ms)
+#define DUMMY_TIMER           10
+#define DUMMY_TIME            (70*_100ms)
 
-/*---------------------------Type Definitions--------------------------------*/
+/*---------------------------Type Definitions---------------------------------*/
 typedef enum {
   WAITING = 0,
   LATCHING,
   UNLATCHING
 } state_t;
 
-/*---------------------------Helper Function Prototypes----------------------*/
+/*---------------------------Helper Function Prototypes-----------------------*/
+static void InitHitch(void);
+static void ProcessHitchIO(void);
 static void CalibrateHobbyMotorController(void);
 static void Latch(unsigned char direction);
 static bool IsLatched(void);
@@ -74,12 +91,15 @@ static bool IsLatching(void);
 static bool IsJammed(void);
 static bool IsCurrentLevelUnsafe(void);
 
-/*---------------------------Module Variables--------------------------------*/
+/*---------------------------Module Variables---------------------------------*/
 static state_t state = WAITING;
+// TODO: remove after debugging
+static uint8_t is_jammed = 0;
+static uint8_t is_unlatched = 0;
 
-/*---------------------------Test Harness------------------------------------*/
+/*---------------------------Test Harness-------------------------------------*/
 #ifdef TEST_HITCH
-#include "./ConfigurationBits.h"
+#include "./core/ConfigurationBits.h"
 
 int main(void) {
 	InitHitch();
@@ -94,33 +114,35 @@ int main(void) {
 }
 
 #endif
-/*---------------------------End Test Harness--------------------------------*/
-/*---------------------------Public Function Definitions---------------------*/
-void InitHitch(void) {
+/*---------------------------Helper Function Definitions----------------------*/
+static void InitHitch(void) {
   CONFIGURE_HEARTBEAT_PIN(OUTPUT); HEARTBEAT_PIN = 0;
-
+  
+  RXUSBDevice_Init(HITCH_PRODUCT_ID);
+  
 	// initialize any dependent moduels
-	InitTimers();
-	unsigned int analog_bit_mask = ( (1 << TEMP_ANALOG_PIN)     |
+	TMRS_Init();
+	uint16_t analog_bit_mask = ((1 << TEMP_ANALOG_PIN) |
                                    (1 << ACTUATOR_ANALOG_PIN) |
                                    (1 << CURRENT_ANALOG_PIN));
-  InitADC(analog_bit_mask);
-  InitPWM(LATCH_PWM_PIN, T_PWM);
+  ADC_Init(analog_bit_mask);
+  PWM_Init(LATCH_PWM_PIN, 0, T_PWM);
   
   // initialize the latch motor controller
   CONFIG_POWER_BUS(OUTPUT); TURN_POWER_BUS(ON);
   CalibrateHobbyMotorController();
   
 	// prime any timers that require it
-	StartTimer(HEARTBEAT_TIMER, HEARTBEAT_TIME);
-	StartTimer(TRANSITION_TIMER, 5000);
+	TMRS_StartTimer(HEARTBEAT_TIMER, HEARTBEAT_TIME);
+	TMRS_StartTimer(TRANSITION_TIMER, 5000);
+	TMRS_StartTimer(DUMMY_TIMER, DUMMY_TIME);
 }
 
-void ProcessHitchIO(void) {
+static void ProcessHitchIO(void) {
  	// toggle a pin to indicate normal operation
-	if (IsTimerExpired(HEARTBEAT_TIMER)) {
+	if (TMRS_IsTimerExpired(HEARTBEAT_TIMER)) {
 		HEARTBEAT_PIN ^= 1;
-		StartTimer(HEARTBEAT_TIMER, HEARTBEAT_TIME);
+		TMRS_StartTimer(HEARTBEAT_TIMER, HEARTBEAT_TIME);
 	}
 	
 	/*
@@ -134,16 +156,66 @@ void ProcessHitchIO(void) {
   	Reset();
   }
   */
-  
-  /*
+
   // only update feedback to software as often as needed
+  /*
   if (IsTimerExpired(UPDATE_TIMER)) {
-    REG_HITCH_POSITION = Map(GetADC(ACTUATOR_ANALOG_PIN), 
+    REG_HITCH_POSITION = Map(ADC_GetConversion(ACTUATOR_ANALOG_PIN), 
                              V_UNLATCHED - ACTUATOR_HYSTERESIS,
                              V_LATCHED + ACTUATOR_HYSTERESIS, 0, 100);
     StartTimer(UPDATE_TIMER, UPDATE_TIME);
   }
+  */
+  /*
+  static uint8_t dummyDesiredPosition = 0;
+  if (TMRS_IsTimerExpired(DUMMY_TIMER)) {
+    TMRS_StartTimer(DUMMY_TIMER, DUMMY_TIME);
+    dummyDesiredPosition ^= 1;
+  }
   
+  // latch as required
+  switch (state) {
+    case WAITING:
+      if (TMRS_IsTimerExpired(TRANSITION_TIMER)) {
+        if (dummyDesiredPosition && IsLatched()) {
+      	  Latch(OFF);
+      	  state = UNLATCHING;
+      	} else if (!(dummyDesiredPosition) && IsUnlatched()) {
+      	  Latch(ON);
+      	  state = LATCHING;
+      	} else if (IsLatching()) {
+      	  // latch as software desires
+      	  if (dummyDesiredPosition) {
+        	  Latch(OFF);
+      	    state = UNLATCHING;
+          } else {
+        	  Latch(ON);
+      	    state = LATCHING;
+      	  }
+        }
+      }
+  	  break;
+  	case LATCHING:
+  	  if (IsLatched() || IsJammed()) {
+        PWM_UpdateDutyCycle(LATCH_PWM_PIN, NEUTRAL_DC);
+        TMRS_StartTimer(TRANSITION_TIMER, TRANSITION_TIME);
+        state = WAITING;
+      }
+      break;
+    case UNLATCHING:
+      if (IsUnlatched() || IsJammed()) {
+        PWM_UpdateDutyCycle(LATCH_PWM_PIN, NEUTRAL_DC);
+        TMRS_StartTimer(TRANSITION_TIMER, TRANSITION_TIME);
+        state = WAITING;
+      }
+      break;
+  	default:
+  	  TMRS_StartTimer(HEARTBEAT_TIMER, 65000); // indicate an error
+  		break;
+  }
+  */
+  
+  /*
   // latch as required
   switch (state) {
     case WAITING:
@@ -185,10 +257,10 @@ void ProcessHitchIO(void) {
   		break;
   }
   */
-
-
+  
+  /*
   // basic test
-  unsigned int dummy = GetADC(ACTUATOR_ANALOG_PIN);
+  unsigned int dummy = ADC_GetConversion(ACTUATOR_ANALOG_PIN);
   if (IsTimerExpired(TRANSITION_TIMER)) {
 		HEARTBEAT_PIN ^= 1;
 		
@@ -208,13 +280,15 @@ void ProcessHitchIO(void) {
 		    break;
     }
 		
-		StartTimer(TRANSITION_TIMER, 2000);
+		StartTimer(TRANSITION_TIMER, 500);
 	}
-
+  */
+  
+  RXUSBDevice_ProcessMessage();
 }
 
-/*---------------------------Helper Function Definitions---------------------*/
-/*****************************************************************************
+
+/******************************************************************************
 Function: CalibrateHobbyMotorController()
 Description: Performs the calibration sequence as defined by the Losi8750KV 
  Hobby sensorless BLDC Motor and controller.
@@ -222,13 +296,13 @@ Notes:
   - http://losi.com/Products/Default.aspx?ProdID=LOSB9594
   - WARNING: must pause at nuetral for some period of time before changing 
     direction for hobby controller to recognize the change
-******************************************************************************/
+*******************************************************************************/
 static void CalibrateHobbyMotorController(void) {
   // wait in nuetral long enough for the controller (beeps when ready)
-  UpdateDutyCycle(NEUTRAL_DC);
+  PWM_UpdateDutyCycle(LATCH_PWM_PIN, NEUTRAL_DC);
 }
   
-/*****************************************************************************
+/*******************************************************************************
 Function: Latch()
 Notes:
   - use the linear potentiometer to realize when we've reached our destination
@@ -236,29 +310,32 @@ Notes:
     move in that direction until it first moves in the opposite direction
   - avoid changing direction faster than the Hobby controller can handle 
     (must pause for ~1s before can change direction)
-******************************************************************************/
+*******************************************************************************/
 static void Latch(unsigned char direction) {
-  StartTimer(TRANSITION_TIMER, TRANSITION_TIME);
-  StartTimer(JAM_TIMER, JAM_CHECK_PERIOD);
+  TMRS_StartTimer(TRANSITION_TIMER, TRANSITION_TIME);
+  TMRS_StartTimer(JAM_TIMER, JAM_CHECK_PERIOD);
   
-  if (direction == ON) UpdateDutyCycle(MAX_ALLOWABLE_DC);
-  else if (direction == OFF) UpdateDutyCycle(MIN_ALLOWABLE_DC);
+  if (direction == ON) PWM_UpdateDutyCycle(LATCH_PWM_PIN, MAX_ALLOWABLE_DC);
+  else if (direction == OFF) PWM_UpdateDutyCycle(LATCH_PWM_PIN, MIN_ALLOWABLE_DC);
 }
 
 
 static bool IsJammed(void) {
   // check whether the potentiometer is NOT changing as quickly as it should
-  if (IsTimerExpired(JAM_TIMER)) {
-    StartTimer(JAM_TIMER, JAM_CHECK_PERIOD);
-    static unsigned int last_position = 0;  // OK because we never reach 0 on the pot
-    unsigned int current_position = GetADC(ACTUATOR_ANALOG_PIN);
+  if (TMRS_IsTimerExpired(JAM_TIMER)) {
+    TMRS_StartTimer(JAM_TIMER, JAM_CHECK_PERIOD);
+    static uint16_t last_position = 0;  // OK because we never reach 0 on the pot
+    uint16_t current_position = ADC_GetConversion(ACTUATOR_ANALOG_PIN);
   
-    unsigned int delta_position = abs((signed int)current_position - (signed int)last_position);
+    uint16_t delta_position = abs((int16_t)current_position - (int16_t)last_position);
     last_position = current_position;
-  
+    
+    if (delta_position < EXPECTED_DELTA_POT) is_jammed = 1;  // TODO: remove after debugged
+    
     return (delta_position < EXPECTED_DELTA_POT);
   }
   
+  is_jammed = 0;  // TODO: remove after debugging
   return 0;  
 }
 
@@ -268,15 +345,15 @@ Description: Determines the state of the latch from linear potentiometer
   limits to take into tolerance in the locking mechanism sliding back.
 */
 static bool IsLatched(void) {
-  return (GetADC(ACTUATOR_ANALOG_PIN) <= V_LATCHED);
+  return (ADC_GetConversion(ACTUATOR_ANALOG_PIN) <= V_LATCHED);
 }
 
 static bool IsUnlatched(void) {
-  return (V_UNLATCHED <= GetADC(ACTUATOR_ANALOG_PIN));
+  return (V_UNLATCHED <= ADC_GetConversion(ACTUATOR_ANALOG_PIN));
 }
 
 static bool IsLatching(void) {
-  unsigned int position = GetADC(ACTUATOR_ANALOG_PIN);
+  unsigned int position = ADC_GetConversion(ACTUATOR_ANALOG_PIN);
   return ((V_LATCHED < position) && (position < V_UNLATCHED));
 }
 
@@ -287,12 +364,9 @@ Description: Returns whether the current is at an unsafe level, rejecting any
   the concept of a watchdog timer.
 */
 static bool IsCurrentLevelUnsafe(void) {
-  if (UNSAFE_CURRENT_LEVEL < GetADC(CURRENT_ANALOG_PIN)) {
-    StartTimer(UNSAFE_CURRENT_TIMER, UNSAFE_CURRENT_TIME);
+  if (UNSAFE_CURRENT_LEVEL < ADC_GetConversion(CURRENT_ANALOG_PIN)) {
+    TMRS_StartTimer(UNSAFE_CURRENT_TIMER, UNSAFE_CURRENT_TIME);
   }
   
-  return (IsTimerExpired(UNSAFE_CURRENT_TIMER));
+  return (TMRS_IsTimerExpired(UNSAFE_CURRENT_TIMER));
 }
-  
-
-/*---------------------------End of File-------------------------------------*/
