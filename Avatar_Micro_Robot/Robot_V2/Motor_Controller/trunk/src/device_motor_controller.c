d3e5c3c8faad3e9e9f246c619c034ca44ae8981a
Taylor <Taylor@0a8336b3-7eeb-364f-9013-38a028100c4a>
2012-11-06 01:31:29 +0000

    Motor spins in one direction.

--- Avatar_Micro_Robot/Robot_V2/Motor_Controller/trunk/src/device_motor_controller.c
+++ Avatar_Micro_Robot/Robot_V2/Motor_Controller/trunk/src/device_motor_controller.c
@@ -17,6 +17,10 @@
 #define CHI_1_ON(a) (_LATD4 = a)
 #define CLO_1_ON(a) (_LATD5 = a)
 
+#define HA_1()      (_RE5)
+#define HB_1()      (_RE6)
+#define HC_1()      (_RE7)
+
 //RP23
 #define BHI_1_OR _RP23R
 #define CHI_1_OR _RP25R
@@ -24,25 +28,32 @@
 
 static void init_pwm(void);
 static void set_pwm_duty(unsigned int duty_cycle);
+static void handle_commutation(void);
+
+static void Timer2_ISR(void);
+static unsigned int fifty_us_counter = 0;
+static unsigned int millisecond_counter = 0;
 
 void Device_Motor_Controller_Process_IO(void)
 {
 
   unsigned int i;
-
+/*
   //for some reason, low side must go high to charge back up
   //high side gate driver
-
-  AHI_1_ON(1);
-  //set_pwm_duty(50);
+  BLO_1_ON(0);
+  //AHI_1_ON(1);
+  set_pwm_duty(50);
   block_ms(2000);
-  AHI_1_ON(0);
+  //AHI_1_ON(0);
 //ALO_1_ON(1);
-  //set_pwm_duty(0);
+  set_pwm_duty(0);
+  BLO_1_ON(1);
   block_ms(2000);
 //ALO_1_ON(0);
   //BLO_1_ON(0);
-
+*/
+handle_commutation();
 
 
 
@@ -66,6 +77,15 @@ void Device_Motor_Controller_Init(void)
   CLO_1_EN(1);
 
   //set_pwm_duty(0);
+
+
+  T2InterruptUserFunction = Timer2_ISR;
+  //T2 frequency is Fosc/2 = 16 MHz
+  //Max timer interrupt at 2^16/16MHz = 4.096ms
+  //Let's set PR2 to 800, to get interrupts every 50us:
+  PR2 = 80;
+  _T2IE = 1;
+  T2CONbits.TON = 1;
 
   block_ms(5000);
 
@@ -105,4 +125,155 @@ static void set_pwm_duty(unsigned int duty_cycle)
 		duty_cycle = 100;
   OC1R = duty_cycle*40;
 
+}
+
+static void handle_commutation(void)
+{
+  static unsigned char MOSFET_states_F[7];
+  static unsigned char MOSFET_states_R[7];
+  static char next_commutation_state[14];
+  static unsigned char sensor_data = 0x00;
+  static unsigned int last_millisecond_counter = 0;
+
+  static unsigned char MOSFET_state = 0x00;
+  static unsigned char dummy1, dummy2;
+
+  dummy1 = (MOSFET_state & 0x010000);
+  dummy2 = (MOSFET_state & 0x001000);
+
+  //HA LA HB LB HC LC
+  MOSFET_states_F[0b101] = 0b100001;
+  MOSFET_states_F[0b100] = 0b001001;
+  MOSFET_states_F[0b110] = 0b011000;
+  MOSFET_states_F[0b010] = 0b010010;
+  MOSFET_states_F[0b011] = 0b000110;
+  MOSFET_states_F[0b001] = 0b100100;
+  MOSFET_states_F[0b000] = 0b000000;
+  MOSFET_states_F[0b111] = 0b000000;
+
+  MOSFET_states_R[0b101] = 0b010010;
+  MOSFET_states_R[0b100] = 0b000110;
+  MOSFET_states_R[0b110] = 0b100100;
+  MOSFET_states_R[0b010] = 0b100001;
+  MOSFET_states_R[0b011] = 0b001001;
+  MOSFET_states_R[0b001] = 0b011000;
+  MOSFET_states_R[0b000] = 0b000000;
+  MOSFET_states_R[0b111] = 0b000000;
+
+
+
+  if(fifty_us_counter >= 20)
+  {
+
+    fifty_us_counter = 0;
+    last_millisecond_counter = millisecond_counter;
+
+    sensor_data = ((HA_1()<<2)+(HB_1()<<1)+HC_1()) & 0b111;
+    
+    MOSFET_state = MOSFET_states_F[sensor_data];
+  
+    if(MOSFET_state & 0b010000)
+    {
+      AHI_1_ON(0);
+      ALO_1_ON(1);
+    }
+    else if(MOSFET_state & 0b100000)
+    {
+      ALO_1_ON(0);
+      AHI_1_ON(1);
+    }
+    else
+    {
+      AHI_1_ON(0);
+      ALO_1_ON(0);
+    }
+  
+    if(MOSFET_state & 0b000100)
+    {
+      BHI_1_ON(0);
+      BLO_1_ON(1);
+    }
+    else if(MOSFET_state & 0b001000)
+    {
+      BLO_1_ON(0);
+      BHI_1_ON(1);
+    }
+    else
+    {
+      BLO_1_ON(0);
+      BHI_1_ON(0);
+    }
+    
+    if(MOSFET_state & 0b000001)
+    {
+      CHI_1_ON(0);
+      CLO_1_ON(1);
+    }
+    else if(MOSFET_state & 0b000010)
+    {
+      CLO_1_ON(0);
+      CHI_1_ON(1);
+    }
+    else
+    {
+      CHI_1_ON(0);
+      CLO_1_ON(0);
+    }
+  }
+  //turn on low side if high side is on
+  else if(fifty_us_counter >=19)
+  {
+    if(MOSFET_state & 0b10000)
+    { 
+      AHI_1_ON(0);
+      ALO_1_ON(1);
+    }
+    if(MOSFET_state & 0b001000)
+    {
+      BHI_1_ON(0);
+      BLO_1_ON(1);
+    }
+    if(MOSFET_state & 0b000010)
+    {
+      CHI_1_ON(0);
+      CLO_1_ON(1);
+    }
+
+  }
+
+}
+
+static void Timer2_ISR(void)
+{
+  _T2IF = 0;
+  //millisecond_counter++;
+  fifty_us_counter++;
+
+  if(fifty_us_counter >= 20)
+  {
+    millisecond_counter++;
+  }
+  if(fifty_us_counter > 20000)
+    fifty_us_counter = 0;
+
+  /*
+  
+  //Make sure rollover is at a multiple of 10
+  if(millisecond_counter == 10000)
+    millisecond_counter = 0;
+
+
+  if(millisecond_counter%10 == 0)
+  {
+    ten_millisecond_counter++;
+    if(millisecond_counter%100 == 0)
+    {
+      hundred_millisecond_counter++;
+      if(millisecond_counter%1000 == 0)
+      {
+        second_counter++;
+      }
+     
+    }
+  }*/
 }
