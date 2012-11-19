663bccf4bc5128482e672dc04831d9a030f12283
Taylor <Taylor@0a8336b3-7eeb-364f-9013-38a028100c4a>
2012-11-19 05:50:06 +0000

    Added variable PWM to the high side MOSFETs.  Still draws too much current.

--- Avatar_Micro_Robot/Robot_V2/Motor_Controller/trunk/src/device_motor_controller.c
+++ Avatar_Micro_Robot/Robot_V2/Motor_Controller/trunk/src/device_motor_controller.c
@@ -21,7 +21,12 @@
 #define HB_1()      (_RE6)
 #define HC_1()      (_RE7)
 
+#define set_AHI_1_duty_cycle(a)  (OC1R = 2000-a*2)
+#define set_BHI_1_duty_cycle(a)  (OC2R = 2000-a*2)
+#define set_CHI_1_duty_cycle(a)  (OC3R = 2000-a*2)
+
 //RP23
+#define AHI_1_OR _RP11R
 #define BHI_1_OR _RP23R
 #define CHI_1_OR _RP25R
 
@@ -33,6 +38,8 @@ static void handle_commutation(void);
 static void Timer2_ISR(void);
 static unsigned int fifty_us_counter = 0;
 static unsigned int millisecond_counter = 0;
+
+static void test_pwm(void);
 
 void Device_Motor_Controller_Process_IO(void)
 {
@@ -53,6 +60,7 @@ void Device_Motor_Controller_Process_IO(void)
 //ALO_1_ON(0);
   //BLO_1_ON(0);
 */
+//test_pwm();
 handle_commutation();
 
 
@@ -83,13 +91,15 @@ void Device_Motor_Controller_Init(void)
   //T2 frequency is Fosc/2 = 16 MHz
   //Max timer interrupt at 2^16/16MHz = 4.096ms
   //Let's set PR2 to 800, to get interrupts every 50us:
-  PR2 = 80;
+  PR2 = 800;
   _T2IE = 1;
   T2CONbits.TON = 1;
 
+  init_pwm();
+
   block_ms(5000);
 
-  //init_pwm();
+
 
 
 }
@@ -100,21 +110,51 @@ static void init_pwm(void)
 	//Choose 400Hz (2.5ms period)
 	//2.5ms = [PR2 + 1]*62.5ns*1
   //CHI_1_OR = 18; //OC1
-  BHI_1_OR = 18;
-	PR2 = 40001;	
-	OC1RS = 4000;
+  //Choose 8kHz (125us period)
+  //125us = (PR2 + 1)*62.5ns
+  //PR2 = 1999
+  AHI_1_OR = 18;
+  BHI_1_OR = 19;
+  CHI_1_OR = 20;
+	//PR2 = 40001;	
+
+  //if both are 0, module doesn't stay low
+  OC1R = 0;
+	OC1RS = 2001;
+
+  OC2R = 0;
+  OC2RS = 2001;
+  
+  OC3R = 0;
+  OC3RS = 2001;
+
+  PR3 = 8000;
 
 
-	OC1CON2bits.SYNCSEL = 0x1f;	
-	//use timer 2
+  //synchronize to Timer 3
+	/*OC1CON2bits.SYNCSEL = 0b1101;	
+	OC2CON2bits.SYNCSEL = 0b1101;	
+	OC3CON2bits.SYNCSEL = 0b1101;	*/
+OC1CON2bits.SYNCSEL = 0x1f;	
+	OC2CON2bits.SYNCSEL = 0x1f;	
+	OC3CON2bits.SYNCSEL = 0x1f;
+	//use timer 3
 
-	OC1CON1bits.OCTSEL2 = 0;
+  //Note:  We can't use PWM, as it is double-buffered
+  //(to avoid shoot-through)
+	//single-compare single-shot (starts high)
+	OC1CON1bits.OCM = 5;
+	OC2CON1bits.OCM = 5;
+	OC3CON1bits.OCM = 5;
 
-	//edge-aligned pwm mode
-	OC1CON1bits.OCM = 6;
+	OC1CON1bits.OCTSEL = 1;
+	OC2CON1bits.OCTSEL = 1;
+	OC3CON1bits.OCTSEL = 1;
 
-	//turn on timer 2
-	T2CONbits.TON = 1;
+
+
+	//turn on timer 3
+	T3CONbits.TON = 1;
 
 
 }
@@ -129,25 +169,46 @@ static void set_pwm_duty(unsigned int duty_cycle)
 
 static void handle_commutation(void)
 {
-  static unsigned char MOSFET_states_F[7];
-  static unsigned char MOSFET_states_R[7];
+  static unsigned char MOSFET_states_F[8];
+  static unsigned char MOSFET_states_F_ATMEL[8];
+  static unsigned char MOSFET_states_R[8];
   static char next_commutation_state[14];
   static unsigned char sensor_data = 0x00;
   static unsigned int last_millisecond_counter = 0;
 
-  static unsigned char MOSFET_state = 0x00;
-  static unsigned char dummy1, dummy2;
+  static unsigned int speed = 0;
 
-  dummy1 = (MOSFET_state & 0x010000);
-  dummy2 = (MOSFET_state & 0x001000);
+  static unsigned char MOSFET_state = 0x00;
+
+  static unsigned char last_MOSFET_state = 0x00;
+  static unsigned int locked_rotor_counter = 0;
+  static unsigned int locked_rotor_recovery_counter = 0;
+  static unsigned int locked_rotor_flag = 0;
+
+  static unsigned int move_counter = 0;
+  static unsigned int last_fifty_us_counter = 0;
 
   //HA LA HB LB HC LC
+  MOSFET_states_F_ATMEL[0b101] = 0b100100;
+  MOSFET_states_F_ATMEL[0b100] = 0b100001;
+  MOSFET_states_F_ATMEL[0b110] = 0b001001;
+  MOSFET_states_F_ATMEL[0b010] = 0b011000;
+  MOSFET_states_F_ATMEL[0b011] = 0b010010;
+  MOSFET_states_F_ATMEL[0b001] = 0b000110;
+  MOSFET_states_F_ATMEL[0b000] = 0b000000;
+  MOSFET_states_F_ATMEL[0b111] = 0b000000;
   MOSFET_states_F[0b101] = 0b100001;
+
   MOSFET_states_F[0b100] = 0b001001;
+  
   MOSFET_states_F[0b110] = 0b011000;
+  
   MOSFET_states_F[0b010] = 0b010010;
+  
   MOSFET_states_F[0b011] = 0b000110;
+  
   MOSFET_states_F[0b001] = 0b100100;
+
   MOSFET_states_F[0b000] = 0b000000;
   MOSFET_states_F[0b111] = 0b000000;
 
@@ -160,86 +221,165 @@ static void handle_commutation(void)
   MOSFET_states_R[0b000] = 0b000000;
   MOSFET_states_R[0b111] = 0b000000;
 
+  speed = 100;
 
-
-  if(fifty_us_counter >= 20)
+  //turn on low side if high side is on
+  if(fifty_us_counter >=19)
   {
+    if(MOSFET_state & 0b100000)
+    { 
+      //AHI_1_ON(0);
+      set_AHI_1_duty_cycle(0);
+      ALO_1_ON(1);
+    }
+    if(MOSFET_state & 0b001000)
+    {
+      //BHI_1_ON(0);
+      set_BHI_1_duty_cycle(0);
+      BLO_1_ON(1);
+    }
+    if(MOSFET_state & 0b000010)
+    {
+      //CHI_1_ON(0);
+      set_CHI_1_duty_cycle(0);
+      CLO_1_ON(1);
+    }
 
-    fifty_us_counter = 0;
-    last_millisecond_counter = millisecond_counter;
+  }
+  //runs every 50us
+  else if(fifty_us_counter != last_fifty_us_counter)
+  {
+    last_fifty_us_counter = fifty_us_counter;
+
+    //fifty_us_counter = 0;
+
+    if(locked_rotor_flag)
+    {
+      locked_rotor_recovery_counter++;
+      //after 5s, try to restart
+      if(locked_rotor_recovery_counter > 1000)
+      {
+        locked_rotor_flag = 0;
+        locked_rotor_counter = 0;
+        locked_rotor_recovery_counter = 0;
+      }
+      return;
+    }
 
     sensor_data = ((HA_1()<<2)+(HB_1()<<1)+HC_1()) & 0b111;
     
     MOSFET_state = MOSFET_states_F[sensor_data];
+    //sensor input, sensor read once stabilized
+    //0b101 0b010
+    //0b100 0b011
+    //0b110 0b001
+    //0b010 0b101
+    //0b011 0b100
+    //0b001 0b110
+    /*MOSFET_state = MOSFET_states_F[0b001];
+    move_counter++;
+
+    if(move_counter > 2400)
+    {
+      move_counter = 0;
+    }
+    if(move_counter > 2000)
+    {
+    MOSFET_state = MOSFET_states_F[0b011];
+
+    }
+    else if(move_counter > 1600)
+    {
+      MOSFET_state = MOSFET_states_F[0b010];
+  }
+      else if(move_counter > 1200)
+    {
+      MOSFET_state = MOSFET_states_F[0b110];
+
+    }
+    else if(move_counter > 800)
+    {
+      MOSFET_state = MOSFET_states_F[0b100];
+
+    }
+    else if (move_counter > 400)
+    {
+      MOSFET_state = MOSFET_states_F[0b101];
+    }*/
+
+    if(MOSFET_state == last_MOSFET_state)
+    {
+      locked_rotor_counter++;
+    }
+    else
+    {
+      locked_rotor_counter = 0;
+    }
+
+    last_MOSFET_state = MOSFET_state;
+
+    //rotors are locked after 200ms
+    if(locked_rotor_counter > 200)
+    {
+      locked_rotor_flag = 1;
+      MOSFET_state = 0x00;
+    }
   
     if(MOSFET_state & 0b010000)
     {
-      AHI_1_ON(0);
+      set_AHI_1_duty_cycle(0);;
       ALO_1_ON(1);
     }
     else if(MOSFET_state & 0b100000)
     {
       ALO_1_ON(0);
-      AHI_1_ON(1);
+      //AHI_1_ON(1);
+      set_AHI_1_duty_cycle(speed);
     }
     else
     {
-      AHI_1_ON(0);
+      set_AHI_1_duty_cycle(0);
       ALO_1_ON(0);
     }
   
     if(MOSFET_state & 0b000100)
     {
-      BHI_1_ON(0);
+      set_BHI_1_duty_cycle(0);
       BLO_1_ON(1);
     }
     else if(MOSFET_state & 0b001000)
     {
       BLO_1_ON(0);
-      BHI_1_ON(1);
+      //BHI_1_ON(1);
+      set_BHI_1_duty_cycle(speed);
     }
     else
     {
       BLO_1_ON(0);
-      BHI_1_ON(0);
+      set_BHI_1_duty_cycle(0);;
     }
     
     if(MOSFET_state & 0b000001)
     {
-      CHI_1_ON(0);
+      set_CHI_1_duty_cycle(0);
       CLO_1_ON(1);
     }
     else if(MOSFET_state & 0b000010)
     {
       CLO_1_ON(0);
-      CHI_1_ON(1);
+      //CHI_1_ON(1);
+      set_CHI_1_duty_cycle(speed);
     }
     else
     {
-      CHI_1_ON(0);
+      set_CHI_1_duty_cycle(0);
       CLO_1_ON(0);
     }
   }
-  //turn on low side if high side is on
-  else if(fifty_us_counter >=19)
-  {
-    if(MOSFET_state & 0b10000)
-    { 
-      AHI_1_ON(0);
-      ALO_1_ON(1);
-    }
-    if(MOSFET_state & 0b001000)
-    {
-      BHI_1_ON(0);
-      BLO_1_ON(1);
-    }
-    if(MOSFET_state & 0b000010)
-    {
-      CHI_1_ON(0);
-      CLO_1_ON(1);
-    }
 
-  }
+  if(fifty_us_counter > 20)
+    fifty_us_counter = 0;
+
 
 }
 
@@ -276,4 +416,27 @@ static void Timer2_ISR(void)
      
     }
   }*/
+}
+
+static void test_pwm(void)
+{
+  ALO_1_ON(0);
+  BLO_1_ON(0);
+  CLO_1_ON(0);
+
+  while(1)
+  {
+
+  set_AHI_1_duty_cycle(500);
+  set_BHI_1_duty_cycle(500);
+  set_CHI_1_duty_cycle(500);
+
+  block_ms(1000);
+
+  set_AHI_1_duty_cycle(0);
+  set_BHI_1_duty_cycle(0);
+  set_CHI_1_duty_cycle(0);
+  block_ms(1000);
+  }
+
 }
