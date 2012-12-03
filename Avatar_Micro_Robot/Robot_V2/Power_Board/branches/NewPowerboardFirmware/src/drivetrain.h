0c7101d265fb6d8cbb01c75c1dd1c95bb3cb30a6
SLeventis <SLeventis@0a8336b3-7eeb-364f-9013-38a028100c4a>
2012-12-03 18:28:07 +0000

    initial commit of drivetrain module
      - refactor out logic for driving the motors and actuating the flipper
        (that PIC24F family does not have a fixed hardware stack size like those in PIC18/16/12/10’s)

---
+++ Avatar_Micro_Robot/Robot_V2/Power_Board/branches/NewPowerboardFirmware/src/drivetrain.h
@@ -0,0 +1,44 @@
+/*==============================================================================
+File: drivetrain.h
+ 
+Description: This module provides an interface to the drivetrain on the robot.
+
+// TODO: implement later, for user level
+
+Responsible Engineer: Stellios Leventis (sleventis@robotex.com)
+==============================================================================*/
+#ifndef DRIVETRAIN_H
+#define DRIVETRAIN_H
+
+#include "./core/StandardHeader.h"
+
+// supported motor options
+typedef enum {
+  kMotorLeft = 0,
+  kMotorRight,
+  kMotorFlipper,
+} kMotor;
+
+/*---------------------------Public Functions---------------------------------*/
+// Function: DT_Init
+// Description: Initializes the given motor.
+void DT_Init(const kMotor motor);
+
+// Function: DT_Run
+// Description: Keeps any internal operations running, most notably timing-out
+//   the feedback speed.  Place this in the main loop.
+void DT_Run(void);
+
+// Function: DR_set_speed
+// Parameters:
+//   const kMotor motor,  the motor on which to operate
+//   const float speed,   the speed effort to apply (0.0-to-1.0)
+//                        negative speed is interpreted as reverse
+void DT_set_speed(const kMotor motor, const float speed);
+
+// Function: DT_speed
+// Description: Getter to retreive the last-updated actual speed of the given
+//   motor.
+float DT_speed(const kMotor motor);
+
+#endif
