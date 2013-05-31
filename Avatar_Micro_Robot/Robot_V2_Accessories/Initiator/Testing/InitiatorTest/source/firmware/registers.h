7508670be1b321532c3b00cc713dd4bfc91a175a
Taylor <Taylor@0a8336b3-7eeb-364f-9013-38a028100c4a>
2013-05-31 01:18:30 +0000

    Updated registers.h.

--- Avatar_Micro_Robot/Robot_V2_Accessories/Initiator/Testing/InitiatorTest/source/firmware/registers.h
+++ Avatar_Micro_Robot/Robot_V2_Accessories/Initiator/Testing/InitiatorTest/source/firmware/registers.h
@@ -14,7 +14,7 @@ Notes:
   - REGISTER(MY_REGISTER_NAME, SOFTWARE_R/W_DIRECTION, 
              SYNC_OR_NO_SYNC, my_data_type)
   
-Responsible Engineer: Stellios Leventis (sleventis@robotex.com)
+Responsible Engineer(s):
 =============================================================================*/
 #ifndef REGISTERS_H
 #define REGISTERS_H
@@ -223,6 +223,31 @@ REGISTER( REG_ROBOT_RESET_REQUEST,   DEVICE_WRITE,  DEVICE_CARRIER,   SYNC,    u
 //firmware, and the rest come from REG_ROBOT_RESET_REQUEST
 REGISTER( REG_ROBOT_RESET_CODE,   DEVICE_READ,  DEVICE_CARRIER,   SYNC,    uint16_t)
 
+//REGISTER INDEX: 0x0070
+//register that is nonzero whenever the AC Adapter is plugged in
+REGISTER( REG_OCU_AC_POWER,   DEVICE_READ,  DEVICE_OCU,   SYNC,    uint8_t)
+
+//register that is nonzero when radio should be powered down or in reset, and zero
+//during normal operation
+REGISTER( REG_ROBOT_RADIO_RESET,   DEVICE_WRITE,  DEVICE_CARRIER,   SYNC,    uint8_t)
+
+//registers for the boom camera.  These should be used similar to the registers for
+//the PTZ.
+REGISTER( REG_BOOM_VEL_PAN,	DEVICE_WRITE,	DEVICE_BOOM_CAM,	SYNC,	int16_t )
+REGISTER( REG_BOOM_VEL_TILT,	DEVICE_WRITE,	DEVICE_BOOM_CAM,	SYNC,	int16_t )
+REGISTER( REG_BOOM_VEL_ZOOM,	DEVICE_WRITE,	DEVICE_BOOM_CAM,	SYNC,	int16_t )
+REGISTER( REG_BOOM_POWER_DOWN,	DEVICE_WRITE,	DEVICE_BOOM_CAM,	SYNC,	int8_t )
+
+REGISTER( REG_MOTOR_CHARGER_STATE,	DEVICE_READ,	DEVICE_MOTOR,	SYNC,	uint16_t )
+
+REGISTER( REG_CAMERA_BASE_FAN_ON,	DEVICE_WRITE,	DEVICE_PTZ_BASE,	SYNC,	uint8_t )
+REGISTER( REG_CAMERA_ROT_FAN_ON,	DEVICE_WRITE,	DEVICE_PTZ_ROTATION,	SYNC,	uint8_t )
+
+REGISTER( REG_OCU_BLOWER_SPEED,	DEVICE_WRITE,	DEVICE_OCU,	SYNC,	uint8_t )
+
+REGISTER( REG_INITIATOR_CHARGE,	DEVICE_WRITE,	DEVICE_INITIATOR,	SYNC,	uint8_t )
+REGISTER( REG_INITIATOR_FIRE,	DEVICE_WRITE,	DEVICE_INITIATOR,	SYNC,	uint8_t )
+REGISTER( REG_INITIATOR_STATE,	DEVICE_READ,	DEVICE_INITIATOR,	SYNC,	uint16_t )
 
 REGISTER_END()
 
