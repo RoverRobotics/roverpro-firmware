43073d0692c9becc56232dae5f4313c1efa143bf
Taylor <Taylor@0a8336b3-7eeb-364f-9013-38a028100c4a>
2013-05-31 00:29:59 +0000

    Adding files to test initiator.

---
+++ Avatar_Micro_Robot/Robot_V2_Accessories/Initiator/Testing/InitiatorTest/InitiatorTest.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+#!/bin/sh
+
+#get path of current script
+shell_script=`readlink -f "$0"`
+
+# compile the program
+current_folder=`dirname $shell_script`
+cd $current_folder/source
+sudo make
+
+current_folder=`dirname $shell_script`
+cd $current_folder/source
+
+# allow FS PIC to work through hub.  This will break HS device functionality.
+# cd /sys/bus/pci/drivers/ehci_hcd/
+# sudo sh -c 'find ./ -name "0000:00:*" -print| sed "s/\.\///">unbind'
+
+# figure out which device ehci_hcd is using, and remove it
+cd /sys/bus/pci/drivers/ehci_hcd
+for device in *; do
+  if test `echo $device | grep -c "0000:"` -eq 0; then
+    echo "this is not the device we're looking for";
+  else
+    echo "Removing " $device
+    sudo su -c "echo '1' > /sys/bus/pci/devices/$device/remove"
+  fi
+done
+current_folder=`dirname $shell_script`
+
+
+
+
+
+while [ true ]
+do
+user_input=""
+echo "\r\n\r\n\r\n"
+echo "[a]rm Initiator"
+echo "[f]ire Initiator"
+echo "fi[r]mware version"
+echo "[u]sb device check"
+echo "[q]uit"
+read user_input
+
+
+
+
+if [ "$user_input" = "a" ]; then
+sudo ./initiator_testing a
+
+if [ "$user_input" = "f" ]; then
+	echo "Ready to fire? [y/n]"
+	read user_input
+	if [ "$user_input" = "y" ]; then
+		sudo ./initiator_testing f
+
+elif [ "$user_input" = "r" ]; then
+echo "\r\n\r\nFirmware Version:\r\n\r\n"
+sudo lsusb -v -d 2694:0014 | grep "RoboteX" -A 2
+
+elif [ "$user_input" = "u" ]; then
+hitch_pic_id=`lsusb | grep -o "2694:0014"`
+if test -n "hitch_pic_id"; then
+echo "Initiator PIC is found"
+else
+echo "Initiator PIC is not found"; 
+fi
+
+
+elif [ "$user_input" = "q" ]; then
+break
+
+else
+echo "\r\n\r\nInvalid input.  Try again.\r\n"
+fi
+
+done
