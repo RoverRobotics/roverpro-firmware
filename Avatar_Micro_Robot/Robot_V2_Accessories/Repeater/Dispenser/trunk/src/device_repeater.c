21a51342fad0db726161fad30d01f4e287cc0cd4
Taylor <Taylor@0a8336b3-7eeb-364f-9013-38a028100c4a>
2013-07-18 02:34:11 +0000

    Copied over initiator firmware, and made some slight changes.

---
+++ Avatar_Micro_Robot/Robot_V2_Accessories/Repeater/Dispenser/trunk/src/device_repeater.c
@@ -0,0 +1,25 @@
+/*---------------------------Dependencies-------------------------------------*/
+#include "device_repeater.h"
+#include "stdhdr.h"
+#include "debug_uart.h"
+
+#define USB_TIMEOUT_ENABLED
+/*---------------------------Macros-------------------------------------------*/
+
+/*---------------------------Helper Function Prototypes-----------------------*/
+
+/*---------------------------Module Variables---------------------------------*/
+
+unsigned char USB_timeout_counter = 0;
+
+/*---------------------------Public Function Definitions----------------------*/
+
+void Repeater_Init()
+{
+
+}
+
+void Repeater_Process_IO()
+{
+
+}
---
+++ Avatar_Micro_Robot/Robot_V2_Accessories/Repeater/Dispenser/trunk/src/device_repeater.h
@@ -0,0 +1,5 @@
+void Repeater_Init();
+
+void Repeater_Process_IO();
+
+extern unsigned char USB_timeout_counter;
