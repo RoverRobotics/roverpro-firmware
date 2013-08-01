2b2c30e866c9ea5b999c0ac68bf8475600d1b1a2
Taylor <Taylor@0a8336b3-7eeb-364f-9013-38a028100c4a>
2013-08-01 02:46:51 +0000

    Added one-letter options to burn-in initiator and tips, removed unused options from script.

--- Avatar_Micro_Robot/Robot_V2_Accessories/Initiator/Testing/InitiatorTest/InitiatorTest.sh
+++ Avatar_Micro_Robot/Robot_V2_Accessories/Initiator/Testing/InitiatorTest/InitiatorTest.sh
@@ -37,10 +37,12 @@ while [ true ]
 do
 user_input=""
 echo "\r\n\r\n\r\n"
-echo "[a]rm Initiator"
-echo "[f]ire Initiator"
+#echo "[a]rm Initiator"
+#echo "[f]ire Initiator"
+#echo "[o]ne shot"
+echo "[t]ip burn-in"
+echo "[i]nitiator burn-in"
 echo "[c]ycle a number of times"
-echo "[o]ne shot"
 echo "fi[r]mware version"
 echo "[u]sb device check"
 echo "[q]uit"
@@ -49,25 +51,31 @@ read user_input
 
 
 
-if [ "$user_input" = "a" ]; then
-sudo ./initiator_testing a
+#if [ "$user_input" = "a" ]; then
+#sudo ./initiator_testing a
 
-elif [ "$user_input" = "f" ]; then
-	echo "Ready to fire? [y/n]"
-	read user_input
-	if [ "$user_input" = "y" ]; then
-		sudo ./initiator_testing f
-	fi
-
-
-elif [ "$user_input" = "o" ]; then
-sudo ./initiator_testing o
+#elif [ "$user_input" = "f" ]; then
+#	echo "Ready to fire? [y/n]"
+#	read user_input
+#	if [ "$user_input" = "y" ]; then
+#		sudo ./initiator_testing f
+#	fi
 
 
-elif [ "$user_input" = "c" ]; then
+#elif [ "$user_input" = "o" ]; then
+#sudo ./initiator_testing o
+
+
+if [ "$user_input" = "c" ]; then
 	echo "Enter number of times to fire:"
 	read user_input
 	sudo ./initiator_testing c $user_input
+
+elif [ "$user_input" = "t" ]; then
+	sudo ./initiator_testing c 50
+
+elif [ "$user_input" = "i" ]; then
+	sudo ./initiator_testing c 50
 
 elif [ "$user_input" = "r" ]; then
 echo "\r\n\r\nFirmware Version:\r\n\r\n"
