a1dcf8cd5fdc581646a77e7948d6d2d7ceb508a9
Taylor Penn <taylor@robotex.com>
2013-10-30 15:26:09 -0700

    Changed message size back to 0xff
    Also updated registers.h, since we'll be adding a new register for A3.

--- Avatar_Micro_Robot/Robot_V2/Power_Board/trunk/usb_config.h
+++ Avatar_Micro_Robot/Robot_V2/Power_Board/trunk/usb_config.h
@@ -49,6 +49,8 @@ typedef int16_t DEVICE_NUMBER;
 #define OCU_REAR_CAM		0x11
 #define DEVICE_BOOM_CAM		0x12
 #define BOOM_CAM_EMPIA		0x13
+#define DEVICE_INITIATOR  	0x14
+#define DEVICE_REPEATER     0x15
 
 
 // -------- PROGRAM DEFINES --------
@@ -59,8 +61,8 @@ typedef int16_t DEVICE_NUMBER;
 #define SYNC    1
 #define NO_SYNC 0
 
-#define OUT_PACKET_LENGTH 0xA0
-#define IN_PACKET_LENGTH  0xA0
+#define OUT_PACKET_LENGTH 0xFF
+#define IN_PACKET_LENGTH  0xFF
 
 #define PACKET_TERMINATOR 0xFFFF
 
