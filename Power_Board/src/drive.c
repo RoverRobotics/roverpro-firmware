0ee465299269230de820d26711add4dc5458c686
Dan Rose <dan@digilabs.io>
2019-04-18 14:36:01 -0500

    Now, when overcurrent is hit, reset the speed smoothing filter so as not to rapidly start-stop. Reduce acceleration limit by an order of magnitude.

--- Power_Board/src/drive.c
+++ Power_Board/src/drive.c
@@ -13,41 +13,41 @@ void drive_tick() {
     uint64_t t0 = g_state.drive.last_update_time;
     uint64_t t1 = g_state.drive.last_update_time = clock_now();
     bool overcurrent = g_state.power.overcurrent;
-    float max_delta_effort =
-        (float)(t1 - t0) * (1.0F / CLOCK_S) / g_settings.drive.time_to_full_speed;
-    max_delta_effort = min(max_delta_effort, g_settings.drive.max_instantaneous_delta_effort);
+
     MotorChannel c;
     for (EACH_MOTOR_CHANNEL(c)) {
-        float duty;
+		float effort;
         MotorStatusFlag motor_flag;
         if (overcurrent) {
-            duty = 0.F;
+			effort = 0.0F;
             motor_flag = MOTOR_FLAG_COAST;
         } else {
             float last_effort = g_state.drive.last_motor_effort[c];
-            float smoothed_effort =
-                clamp(g_state.communication.motor_effort[c], last_effort - max_delta_effort,
+			float max_delta_effort = min(
+				(float)(t1 - t0) * (1.0F / CLOCK_S) / g_settings.drive.time_to_full_speed,
+				g_settings.drive.max_instantaneous_delta_effort
+			);
+            effort = clamp(g_state.communication.motor_effort[c], last_effort - max_delta_effort,
                       last_effort + max_delta_effort);
-            g_state.drive.last_motor_effort[c] = smoothed_effort;
 
             motor_flag = MOTOR_FLAG_NONE;
-            if ((smoothed_effort < 0.0F) ^ (c == MOTOR_RIGHT)) {
+            if ((effort < 0.0F) ^ (c == MOTOR_RIGHT)) {
                 motor_flag |= MOTOR_FLAG_REVERSE;
             }
-            if (smoothed_effort == 0.0F) {
-                motor_flag |=
-                    g_state.communication.brake_when_stopped ? MOTOR_FLAG_BRAKE : MOTOR_FLAG_COAST;
+            if (effort == 0.0F && g_state.communication.brake_when_stopped) {
+                motor_flag |= MOTOR_FLAG_BRAKE;
             }
             if (g_settings.drive.motor_slow_decay_mode) {
-                // in slow decay mode, 0 duty = reverse, 0.5 = still, 1 = forward
-                duty = fabsf(smoothed_effort) * 0.5F + 0.5F;
                 motor_flag |= MOTOR_FLAG_DECAY_MODE;
-            } else {
-                duty = fabsf(smoothed_effort);
             }
         }
+		// in fast (normal) decay mode, 0 duty = still, 1 = forward
+        // in slow decay mode, 0 duty = reverse, 0.5 = still, 1 = forward
+		float duty = (motor_flag & MOTOR_FLAG_DECAY_MODE) ? fabsf(effort) * 0.5F + 0.5F : fabsf(effort);
         g_state.drive.motor_status[c] = motor_update(c, motor_flag, duty);
+		g_state.drive.last_motor_effort[c] = effort;
     }
+
     tach_tick();
 }
 
@@ -56,7 +56,8 @@ void drive_init() {
     g_state.drive.last_update_time = clock_now();
 
     for (EACH_MOTOR_CHANNEL(c)) {
-        g_state.drive.last_motor_effort[c] = 0.0F;
+		g_state.drive.motor_status[c] = motor_update(c, MOTOR_FLAG_COAST, 0.0F);
+		g_state.drive.last_motor_effort[c] = 0.0F;
         motor_init(c);
     }
     motor_tach_init();
