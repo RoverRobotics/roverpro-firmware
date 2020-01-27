356a6a550d52a80c4eabf104f5c2fc2bcabbf9ea
Dan Rose <dan@digilabs.io>
2020-01-26 23:02:36 -0600

    Clean up linker options
    also fix compatibility with debugger

--- cmake/PowerBoard_toolchain.cmake
+++ cmake/PowerBoard_toolchain.cmake
@@ -33,51 +33,35 @@ else()
   set(XC16_ROOT "/opt/microchip/xc16/${XC16_VER}")
 endif()
 find_package(XC16)
-set(CMAKE_EXECUTABLE_SUFFIX_C .elf)
-set(CMAKE_EXECUTABLE_FORMAT ELF)
-
-set(ENV{XC16_OMF} ${CMAKE_EXECUTABLE_FORMAT})
 
 # while xc16-gcc.exe doesn't play nice with feature detection, coff-gcc and elf-gcc do.
 # as per https://www.microchip.com/forums/m1099635.aspx
 set(CMAKE_C_COMPILER "${XC16_elf-gcc_EXECUTABLE}")
-set(CMAKE_C_STANDARD 99)
-add_link_options(LINKER:-p${MICROCHIP_CPU_ID},--report-mem
-  LINKER:--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D__DEBUG=__DEBUG,--defsym=__MPLAB_DEBUGGER_PK3=1
-  -mreserve=data@0x800:0x850)
-#
-#make -f nbproject/Makefile-XC16_24FJ256GA110.mk SUBPROJECTS= .build-conf
-#make[1]: Entering directory 'C:/Users/dan/MPLABXProjects/pic24_c_template_1.X'
-#make  -f nbproject/Makefile-XC16_24FJ256GA110.mk dist/XC16_24FJ256GA110/debug/pic24_c_template_1.X.debug.elf
-#make[2]: Entering directory 'C:/Users/dan/MPLABXProjects/pic24_c_template_1.X'
-#"C:\opt\Microchip\xc16\v1.41\bin\xc16-gcc.exe"   main.c  -o build/XC16_24FJ256GA110/debug/main.o  -c -mcpu=24FJ256GB106  -MMD -MF "build/XC16_24FJ256GA110/debug/main.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1    -omf=elf -DXPRJ_XC16_24FJ256GA110=XC16_24FJ256GA110  -no-legacy-libc    -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="C:/opt/Microchip/MPLABX/v5.30/packs/Microchip/PIC24F-GA-GB_DFP/1.1.74"/xc16
-#"C:\opt\Microchip\xc16\v1.41\bin\xc16-gcc.exe"   system.c  -o build/XC16_24FJ256GA110/debug/system.o  -c -mcpu=24FJ256GB106  -MMD -MF "build/XC16_24FJ256GA110/debug/system.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1    -omf=elf -DXPRJ_XC16_24FJ256GA110=XC16_24FJ256GA110  -no-legacy-libc    -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="C:/opt/Microchip/MPLABX/v5.30/packs/Microchip/PIC24F-GA-GB_DFP/1.1.74"/xc16
-#"C:\opt\Microchip\xc16\v1.41\bin\xc16-gcc.exe"   interrupts.c  -o build/XC16_24FJ256GA110/debug/interrupts.o  -c -mcpu=24FJ256GB106  -MMD -MF "build/XC16_24FJ256GA110/debug/interrupts.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1    -omf=elf -DXPRJ_XC16_24FJ256GA110=XC16_24FJ256GA110  -no-legacy-libc    -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="C:/opt/Microchip/MPLABX/v5.30/packs/Microchip/PIC24F-GA-GB_DFP/1.1.74"/xc16
-#"C:\opt\Microchip\xc16\v1.41\bin\xc16-gcc.exe"   configuration_bits.c  -o build/XC16_24FJ256GA110/debug/configuration_bits.o  -c -mcpu=24FJ256GB106  -MMD -MF "build/XC16_24FJ256GA110/debug/configuration_bits.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1    -omf=elf -DXPRJ_XC16_24FJ256GA110=XC16_24FJ256GA110  -no-legacy-libc    -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="C:/opt/Microchip/MPLABX/v5.30/packs/Microchip/PIC24F-GA-GB_DFP/1.1.74"/xc16
-#"C:\opt\Microchip\xc16\v1.41\bin\xc16-gcc.exe"   traps.c  -o build/XC16_24FJ256GA110/debug/traps.o  -c -mcpu=24FJ256GB106  -MMD -MF "build/XC16_24FJ256GA110/debug/traps.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1    -omf=elf -DXPRJ_XC16_24FJ256GA110=XC16_24FJ256GA110  -no-legacy-libc    -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="C:/opt/Microchip/MPLABX/v5.30/packs/Microchip/PIC24F-GA-GB_DFP/1.1.74"/xc16
-#"C:\opt\Microchip\xc16\v1.41\bin\xc16-gcc.exe"   user.c  -o build/XC16_24FJ256GA110/debug/user.o  -c -mcpu=24FJ256GB106  -MMD -MF "build/XC16_24FJ256GA110/debug/user.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1    -omf=elf -DXPRJ_XC16_24FJ256GA110=XC16_24FJ256GA110  -no-legacy-libc    -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="C:/opt/Microchip/MPLABX/v5.30/packs/Microchip/PIC24F-GA-GB_DFP/1.1.74"/xc16
-#"C:\opt\Microchip\xc16\v1.41\bin\xc16-gcc.exe"   -o dist/XC16_24FJ256GA110/debug/pic24_c_template_1.X.debug.elf  build/XC16_24FJ256GA110/debug/configuration_bits.o build/XC16_24FJ256GA110/debug/interrupts.o build/XC16_24FJ256GA110/debug/main.o build/XC16_24FJ256GA110/debug/system.o build/XC16_24FJ256GA110/debug/traps.o build/XC16_24FJ256GA110/debug/user.o      -mcpu=24FJ256GB106        -D__DEBUG=__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -omf=elf -DXPRJ_XC16_24FJ256GA110=XC16_24FJ256GA110  -no-legacy-libc     -mreserve=data@0x800:0x81F -mreserve=data@0x820:0x821 -mreserve=data@0x822:0x823 -mreserve=data@0x824:0x825 -mreserve=data@0x826:0x84F   -Wl,,,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D__DEBUG=__DEBUG,--defsym=__MPLAB_DEBUGGER_PK3=1,,--script=p24FJ256GB106.gld,--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="dist/XC16_24FJ256GA110/debug/pic24_c_template_1.X.debug.map",--report-mem,--memorysummary,dist/XC16_24FJ256GA110/debug/memoryfile.xml  -mdfp="C:/opt/Microchip/MPLABX/v5.30/packs/Microchip/PIC24F-GA-GB_DFP/1.1.74"/xc16
-#
+set(CMAKE_C_COMPILER_ID XC16-gcc FORCE)
 
+set(CMAKE_EXECUTABLE_SUFFIX_C .elf)
+set(ENV{XC16_OMF} elf)
+
+set(CMAKE_C_LINKER_WRAPPER_FLAG "-Wl,")
+set(CMAKE_C_LINKER_WRAPPER_FLAG_SEP ",")
+set(CMAKE_EXE_LINKER_FLAGS_INIT
+  -Wl,--defsym=__ICD2RAM=0
+  #__ICD2RAM is used in the linker script to provision RAM for the PICKit3. We want to keep this so we can debug a live build
+  )
 
 # note: -mafrlcsj turns off license check
-set(CMAKE_C_FLAGS_INIT " -no-legacy-libc -mafrlcsj -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mdfp=C:/opt/Microchip/MPLABX/v5.30/packs/Microchip/PIC24F-GA-GB_DFP/1.1.74/xc16")
+set(CMAKE_C_FLAGS_INIT " -mcpu=${MICROCHIP_CPU_ID} -no-legacy-libc -mafrlcsj") #-mcpu=${MICROCHIP_CPU_ID}
 set(CMAKE_C_FLAGS_DEBUG_INIT " -g -D__DEBUG")
 set(CMAKE_C_FLAGS_MINSIZEREL_INIT " -Os -DNDEBUG")
 set(CMAKE_C_FLAGS_RELEASE_INIT " -O3 -DNDEBUG")
 set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT " -O2 -g -DNDEBUG")
 
-set(CMAKE_C_LINKER_WRAPPER_FLAG "-Wl,")
-set(CMAKE_C_LINKER_WRAPPER_FLAG_SEP ",")
-set(CMAKE_C_COMPILER "${XC16_elf-gcc_EXECUTABLE}")
-
 add_compile_options(
-  -std=c99
-  -mcpu=${CMAKE_SYSTEM_PROCESSOR}
   # -mcci #todo: migrate to CCI
-  -no-legacy-libc
   -ffunction-sections
   -fno-short-double
   -Wall
   -fast-math
+  -merrata=psv_trap
+  -no-legacy-libc
 )
