6e432ee334de56287bbe1f053e4e9cda1729943a
Dan Rose <dan@digilabs.io>
2020-02-05 15:05:29 -0600

    simplify pk3 scripts

--- scripts/read_mcu.bat
+++ scripts/read_mcu.bat
@@ -1,13 +1,6 @@
-set pk3cmd=C:\opt\Microchip\MPLAB IDE\Programmer Utilities\PICkit3\PK3CMD.exe
+set pk3cmd="C:\opt\Microchip\MPLAB IDE\Programmer Utilities\PICkit3\PK3CMD.exe"
 set part=24FJ256GB106
-For /f "tokens=1-3 delims=/:." %%a in ("%TIME%") do (set mytime=T%%a%%b%%c)
 
-set scriptdir=%~dp0
-set buildbase=%scriptdir%../build
-set builddir=%buildbase%/ipe
-mkdir "%builddir%" 2>  NUL
+set hex_file=%~dpnx1
 
-PUSHD "%builddir%"
-set old_image=%builddir%/memory-%mytime%.hex
-%pk3cmd% -P%part% -GF "%old_image%" -L
-POPD
+%pk3cmd% -P%part% -GF"%hex_file%" -L
