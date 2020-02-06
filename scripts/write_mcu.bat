9904e3e99fb57ec5947261f8e6e5c30e20bac71e
Dan Rose <dan@digilabs.io>
2020-02-05 18:56:41 -0600

    Add clion runnables flatten docs 1 level

--- scripts/write_mcu.bat
+++ scripts/write_mcu.bat
@@ -2,8 +2,6 @@ set pk3cmd="C:\opt\Microchip\MPLAB IDE\Programmer Utilities\PICkit3\PK3CMD.exe"
 set part=24FJ256GB106
 
 set new_image=%~dpnx1
-set range=0-83ff
-
 echo "writing %1 to device"
 
 %pk3cmd% -P%part% -F"%new_image%" -M -L
