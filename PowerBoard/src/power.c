0217e1469e2274a213ef0fa4d31d8f40cc00810c
Dan Rose <dan@digilabs.io>
2020-08-08 21:38:10 -0500

    Use "new", not "hybrid" powerbus method as fallback

--- PowerBoard/src/power.c
+++ PowerBoard/src/power.c
@@ -7,55 +7,56 @@
 
 /// Turn all the batteries on immediately
 static bool try_turn_on_power_bus_immediate() {
-    if (RCONbits.POR){
-		return false;
-	}
-	// If the system is "warm", assume the power bus is still energized and just switch the power
+    if (RCONbits.POR) {
+        return false;
+    }
+    // If the system is "warm", assume the power bus is still energized and just switch the power
     // bus back on.
-	set_active_batteries(BATTERY_FLAG_ALL);
-	return true;
- }
+    set_active_batteries(BATTERY_FLAG_ALL);
+    return true;
+}
 
 /// Pulse the power bus with quadratic-length pulses
-/// (600 + (i**2 / 8) microseconds on + 40 ms off) x 200
-static void turn_on_power_bus_hybrid_method() {
-    uint32_t i;
-    for (i = 0; i < 200; i++) {
+/// (600 + (i**2 / 8) microseconds on + 10 ms off) x 300
+static void turn_on_power_bus_new_method() {
+    uint32_t i = 0;
+    for (i = 0; i < 300; i++) {
         set_active_batteries(BATTERY_FLAG_ALL);
         block_us(600 + i * i / 8);
         set_active_batteries(BATTERY_FLAG_NONE);
-        block_us(40000);
+        block_us(10000);
     }
     set_active_batteries(BATTERY_FLAG_ALL);
 }
 
 static bool try_turn_on_power_bus_adaptive() {
-	int32_t pulse_on_us = 600;
+    int32_t pulse_on_us = 600;
 	int32_t pulse_max_us = 10000;
    	while (pulse_on_us < pulse_max_us) {
         set_active_batteries(BATTERY_FLAG_ALL);
         block_us(pulse_on_us);
         set_active_batteries(BATTERY_FLAG_NONE);
-		I2CResult result[2] = {0};
-		uint16_t battery_status[2] = {0};
+        I2CResult result[2] = {0};
+        uint16_t battery_status[2] = {0};
 
-		result[0] = i2c_synchronously_await(I2C_BUS2, i2c_op_read_word(BATTERY_ADDRESS, 0x16, &battery_status[0]));
-		result[1] = i2c_synchronously_await(I2C_BUS3, i2c_op_read_word(BATTERY_ADDRESS, 0x16, &battery_status[1]));
+        result[0] = i2c_synchronously_await(
+            I2C_BUS2, i2c_op_read_word(BATTERY_ADDRESS, 0x16, &battery_status[0]));
+        result[1] = i2c_synchronously_await(
+            I2C_BUS3, i2c_op_read_word(BATTERY_ADDRESS, 0x16, &battery_status[1]));
 
-		if (result[0] != I2C_OKAY && result[1] != I2C_OKAY){
-			// we can't communicate with either battery. Abort.
-			return false;
-		}	
-		if ((result[0] == I2C_OKAY && !(battery_status[0] & 0x0800)) ||
-			(result[1] == I2C_OKAY && !(battery_status[1] & 0x0800)))
-		{
-			// either battery reports okay power draw
-			__builtin_clrwdt();
-			pulse_on_us = pulse_on_us * 9 / 8;
-		}
+        if (result[0] != I2C_OKAY && result[1] != I2C_OKAY) {
+            // we can't communicate with either battery. Abort.
+            return false;
+        }
+        if ((result[0] == I2C_OKAY && !(battery_status[0] & 0x0800)) ||
+            (result[1] == I2C_OKAY && !(battery_status[1] & 0x0800))) {
+            // either battery reports okay power draw
+            __builtin_clrwdt();
+            pulse_on_us = pulse_on_us * 9 / 8;
+        }
     }
     set_active_batteries(BATTERY_FLAG_ALL);
-	return true;
+    return true;
 }
 
 void power_init() {
@@ -72,10 +73,9 @@ void power_init() {
     if (try_turn_on_power_bus_immediate()) {
         return;
     }
-	if (try_turn_on_power_bus_adaptive()){
-		return;
-	}
-	else {
-		turn_on_power_bus_hybrid_method();
-	}
+    if (try_turn_on_power_bus_adaptive()) {
+        return;
+    } else {
+        turn_on_power_bus_new_method();
+    }
 }
