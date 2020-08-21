d85c0f5af715ffe0c3420788877c771f41eadba8
Dan Rose <dan@digilabs.io>
2020-08-21 17:04:22 -0500

    tidying?

--- PowerBoard/src/flipper.h
+++ PowerBoard/src/flipper.h
@@ -2,9 +2,8 @@
 /// Translate flipper sensors (potentiometers) from independent readings into a meaningful angular
 /// position measure
 
-#ifndef FLIPPER_H
-#define FLIPPER_H
-#include "stdint.h"
+#pragma once
+#include <stdint.h>
 
 /// Figures out the math needed to interpret potentiometer readings.
 /// Saves this in Settings in NVM, then idles until the rover is turned off.
@@ -12,5 +11,3 @@ __attribute__((noreturn)) void flipper_feedback_calibrate();
 
 /// Computes the actual flipper value and stores the result in _state.drive.flipper_angle
 void tick_flipper_feedback();
-
-#endif
